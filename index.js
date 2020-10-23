127559b643f73614a079cbc2316c26b6cdca794d
andrazpolaktipteh <andrazpolaktipteh@gmail.com>
2020-10-23 18:35:16 +0200

    done for now

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const path = require('path');
+const app_main = express();
+const app_dashboard = express();
+const PORT_DISPLAY = process.env.PORT_DISPLAY || 3004;
+const PORT_DASHBOARD = process.env.PORT_DASHBOARD || 3005;
 
-//app.use('/',express.static('../front_end_testpages'));
-app.use('/',express.static('../front_end'));
+const PATH_DISPLAY = process.env.PATH_DISPLAY || '../tipteh_people_counter_v2/build';
+const PATH_DASHBOARD = process.env.PATH_DASHBOARD || '../tipteh_peoplecounter_frotend_statistic/build';
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app_main.use('/',express.static(path.normalize(PATH_DISPLAY)));
+app_dashboard.use('/',express.static( PATH_DASHBOARD));
+
+app_main.listen(PORT_DISPLAY, () => console.log(`Server running: path = ${PATH_DISPLAY}, port = ${PORT_DISPLAY}`));
+app_dashboard.listen(PORT_DASHBOARD, () => console.log(`Server running: path = ${PATH_DASHBOARD}, port = ${PORT_DASHBOARD}`));
+
